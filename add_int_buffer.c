b73d6bf8ef43b4bdacfdf976cedb5c16e6a794ee
ibtissam zennibi <ibtissamzennibii@gmail.com>
2023-07-24 16:41:45 -0700

    edit

--- add_int_buffer.c
+++ add_int_buffer.c
@@ -1,25 +1,28 @@
 #include "main.h"
-
-
-
+/**
+ * add_int_buffer - insert int buffer
+ * @d: int
+ * @buffer: buffer
+ * @index: actual index
+ */
 int add_int_buffer(int d ,char *buffer,int index)
 {
-	int sign, vtemp, i;
+	unsigned int sign = 0, vtemp, i, j = 0;
 
-	vtemp = d;
 	if (d < 0)
 	{
 		sign = 1;
 		d = d * -1;
 		index = add_to_buffer('-', buffer, index);
 	}
+	vtemp = d;
 	for (i = 1; d > 9; i *= 10)
 		d = d / 10;
-	while (vtemp > 0)
+	while (i > 0)
 	{
-		index = add_to_buffer(48 + (vtemp / i), buffer, index);
-		vtemp = vtemp % i;
+		index = add_to_buffer(48 + ((vtemp / i) % 10), buffer, index);
 		i /= 10;
+		j++;
 	}
-	return (index + sign);
+	return (j + sign);
 }
---
+++ main.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+int main()
+{
+	_printf("foot%ifoot%dfoot%s\n",34,-23,"gool");
+	return 0;
+}
